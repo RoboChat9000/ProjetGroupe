5fe41768ebb11b3c71be1470315c416d67754ace
Mwah <sebastien.broin@gmail.com>
2018-05-24 15:09:43 +0200

    Les cotes !

--- Projet-Paris-Sportif-Model/src/main/java/data/Cote.java
+++ Projet-Paris-Sportif-Model/src/main/java/data/Cote.java
@@ -1,21 +1,41 @@
 package data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Cote")
 public class Cote {
 
-	private int idMatch;
-	private int dom;
-	private int nul;
-	private int ext;
-	private int vainqueurDom;
-	private int vainqueurExt;
 	
-	public Cote(int idMatch, int coteDom, int coteNul, int coteExt, int coteVainqueurDom, int coteVainqueurExt) {
-		this.idMatch = idMatch;
+	@OneToOne
+	@Column(name="COT_MATCH")
+	private Match match;
+	
+	@OneToOne
+	@Column(name="COT_DOM")
+	private int dom;
+	
+	@OneToOne
+	@Column(name="COT_NUL")
+	private int nul;
+	
+	@OneToOne
+	@Column(name="COT_EXT")
+	private int ext;
+	
+/*	private int vainqueurDom;
+	private int vainqueurExt;*/
+	
+	public Cote(Match match, int coteDom, int coteNul, int coteExt /*, int coteVainqueurDom, int coteVainqueurExt*/) {
+		this.match = match;
 		this.dom = dom;
 		this.nul = nul;
 		this.ext = ext;
-		this.vainqueurDom = vainqueurDom;
-		this.vainqueurExt = vainqueurExt;
+/*		this.vainqueurDom = vainqueurDom;
+		this.vainqueurExt = vainqueurExt;*/
 	}
 	
 }
