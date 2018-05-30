5b80e90bd05d6a6bf958d1da80070c80afa82f76
Sami <almukhtar.sami@gmail.com>
2018-05-30 14:59:19 +0200

    IDAO Pari

--- Projet-Paris-Sportif-Spring-JPA/src/main/java/DAO_JPA/IDAOMatch.java
+++ Projet-Paris-Sportif-Spring-JPA/src/main/java/DAO_JPA/IDAOMatch.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import data.Match;
 
 public interface IDAOMatch extends JpaRepository<Match, Integer>{
+	
+	public Match findById(int id);
 
 }
