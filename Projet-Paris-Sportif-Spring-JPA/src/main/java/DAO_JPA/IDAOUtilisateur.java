75702b9855b9bbc7dbad53817c768ccb87c95bda
Gandalf le Git <leo.dayviaud@gmail.com>
2018-05-30 14:29:35 +0200

    update utilisateur dao

--- Projet-Paris-Sportif-Spring-JPA/src/main/java/DAO_JPA/IDAOUtilisateur.java
+++ Projet-Paris-Sportif-Spring-JPA/src/main/java/DAO_JPA/IDAOUtilisateur.java
@@ -1,5 +1,9 @@
 package DAO_JPA;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import data.Utilisateur;
@@ -11,6 +15,19 @@ public interface IDAOUtilisateur extends JpaRepository<Utilisateur, Integer>
 {
 
 	public Utilisateur findById(int id);
+	public List<Utilisateur> findAll();
+	public Utilisateur findByPseudo (String pseudo);
+	
+	//retourne vrai si le pseudo existe deja
+	 @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM Utilisateur u WHERE u.pseudo = :pseudo")
+	    boolean VerifPseudo(@Param("pseudo") String pseudo);
+	
+//	 @Query("INSERT INTO utilisateur_ligue (ul_utilisateur_id, ul_ligue_id) VALUES ((SELECT uti_id FROM utilisateur WHERE uti_id = :idUtil), (SELECT lig_id FROM ligue WHERE lig_id = :idLig))")
+//	 public void rejoinLigue(@Param("idUtil") int idUtil, @Param("idLig") int idLig);
+
+	
+		
+	
 	
 	
 }
