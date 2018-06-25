1b5db131d5060f65c988c6cafa21cb119a01a962
Gandalf le Git <leo.dayviaud@gmail.com>
2018-06-25 09:33:20 +0200

    Update Angular
    init projet angular

---
+++ Projet-Paris-Sportif-Angular/src/app/utilisateur/utilisateur.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UtilisateurComponent]
+})
+export class UtilisateurModule { }
